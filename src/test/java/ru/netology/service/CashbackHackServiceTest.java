6a08a04385133b8c346b23cc9940cf0b6dc0c7f0
Aleksandr <skif5074@outlook.com>
2023-04-03 23:57:54 +0200

    fix test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -3,40 +3,40 @@ package ru.netology.service;
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainBoundaryValuesOne() {
-        org.testng.Assert.assertThrows(RuntimeException.class, () -> {
+        org.junit.Assert.assertThrows(RuntimeException.class, () -> {
             service.remain(-1);
         });
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainBoundaryValuesTwo() {
-        org.testng.Assert.assertEquals(service.remain(0), 1000);
+        org.junit.Assert.assertEquals(1000, service.remain(0));
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainBoundaryValuesThree() {
-        org.testng.Assert.assertEquals(service.remain(1), 999);
+        org.junit.Assert.assertEquals(999, service.remain(1));
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainBoundaryValuesFour() {
-        org.testng.Assert.assertEquals(service.remain(999), 1);
+        org.junit.Assert.assertEquals(1, service.remain(999));
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainBoundaryValuesFive() {
-        org.testng.Assert.assertEquals(service.remain(1000), 0);
+        org.junit.Assert.assertEquals(0, service.remain(1000));
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainBoundaryValuesSix() {
-        org.testng.Assert.assertEquals(service.remain(1001), 999);
+        org.junit.Assert.assertEquals(999, service.remain(1001));
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainCriticalPath() {
-        org.testng.Assert.assertEquals(service.remain(400), 600);
+        org.junit.Assert.assertEquals(600, service.remain(400));
     }
 }
